56c51be2f752ac3e2ff2290ce38b41c0b8c953b5
maxsos19 <126406905+maxsos19@users.noreply.github.com>
2024-09-09 13:06:59 +0300

    Add files via upload

---
+++ MainConnector.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Data.SqlClient;
+using System.Threading.Tasks;
+using AdoNet2._0.Confuguration;
+using System.Data;
+
+namespace AdoNet2._0
+{
+    public class MainConnector
+    {
+        SqlConnection connection = new SqlConnection(ConnectionString.MSSQLConnection);
+        static void Main(string[] args)
+        {
+            var connector = new MainConnector();
+
+            var result = connector.ConnectAsync();
+            if (result.Result)
+            {
+                Console.WriteLine("Подключение успешно!");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка подключения!");
+            }
+            Console.ReadKey();
+
+        }
+        public async Task<bool> ConnectAsync()
+        {
+          
+            bool result;
+            try
+            {
+                connection = new SqlConnection(ConnectionString.MSSQLConnection);
+                await connection.OpenAsync();
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+            return result;
+        }
+        public void DisconnectAsync()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+        public SqlConnection GetConnection()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                return connection;
+            }
+            else
+            {
+                throw new Exception("Подключение уже закрыто!");
+            }
+        }
+    
+
+
+
+
+
+}
+    }
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿
+using AdoNet2._0;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module24Concole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var connector = new MainConnector();
+
+            var result = connector.ConnectAsync();
+
+            var data = new DataTable();
+
+            if (result.Result)
+            {
+                Console.WriteLine("Подключено успешно!");
+
+                var db = new DbExecutor(connector);
+
+                var tablename = "NetworkUser";
+
+                Console.WriteLine("Получаем данные таблицы " + tablename);
+
+                data = db.SelectAll(tablename);
+
+                Console.WriteLine("Количество строк в " + tablename + ": " + data.Rows.Count);
+
+                Console.WriteLine("Отключаем БД!");
+                connector.DisconnectAsync();
+
+                result = connector.ConnectAsync();
+
+                if (result.Result)
+                {
+                    Console.WriteLine("Количество строк в " + tablename + ": " + data.Rows.Count);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Ошибка подключения!");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
